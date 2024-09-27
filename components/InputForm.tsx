e83ac9dccca27a6a6a10a15cda2823785c06a4cb
Utpal Lotlikar <utpal@easyops.in>
2024-09-27 11:53:33 +0530

    UI updated to light theme

--- components/InputForm.tsx
+++ components/InputForm.tsx
@@ -54,199 +54,201 @@ export const InputForm = () => {
 
   return (
     <>
-      <h2 className="text-2xl mt-10">Shopify Plus Migration Cost Calculator</h2>
-      <h4 className="mb-5">
-        Check your saving when you migrate from Shopify Plus Plan to Medusa
-      </h4>
-      {showForm ? (
-        <Form {...form}>
-          {/* {state?.message !== "" && !state.errors && (
+      <div className="border rounded-lg p-6 shadow-sm mt-10">
+        <h2 className="text-2xl">Shopify Plus Cost Comparison</h2>
+        <h4 className="mb-5">
+          Check your saving when you migrate from Shopify Plus Plan to Medusa
+        </h4>
+        {showForm ? (
+          <Form {...form}>
+            {/* {state?.message !== "" && !state.errors && (
             <div className="text-sm font-medium text-green-500">
               {state.message}
             </div>
           )} */}
-          {state?.message !== "" && state.errors && (
-            <div className="text-sm font-medium text-destructive">
-              {state.message}
-            </div>
-          )}
-          <form
-            ref={formRef}
-            action={formAction}
-            onSubmit={(evt) => {
-              evt.preventDefault();
-              form.handleSubmit(() => {
-                console.log("called form submit");
-                formRef.current?.submit();
-              })(evt);
-            }}
-            className="space-y-2"
-          >
-            <div className="flex gap-2">
-              <FormField
-                control={form.control}
-                name="shopify_fees"
-                render={({ field }) => (
-                  <FormItem className="w-full">
-                    <FormLabel>Shopify Plus Cost (Monthly):</FormLabel>
-                    <FormControl>
-                      <div className="flex h-10 items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2">
-                        <DollarSign className="h-[16px] w-[16px]" />
-                        <input
-                          {...field}
+            {state?.message !== "" && state.errors && (
+              <div className="text-sm font-medium text-destructive">
+                {state.message}
+              </div>
+            )}
+            <form
+              ref={formRef}
+              action={formAction}
+              onSubmit={(evt) => {
+                evt.preventDefault();
+                form.handleSubmit(() => {
+                  console.log("called form submit");
+                  formRef.current?.submit();
+                })(evt);
+              }}
+              className="space-y-2"
+            >
+              <div className="flex gap-2">
+                <FormField
+                  control={form.control}
+                  name="shopify_fees"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Shopify Plus Cost (Monthly):</FormLabel>
+                      <FormControl>
+                        <div className="flex h-10 items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2">
+                          <DollarSign className="h-[16px] w-[16px]" />
+                          <input
+                            {...field}
+                            type="number"
+                            className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+                          />
+                        </div>
+                      </FormControl>
+                      {/* <FormDescription>Shopify Plus monthly fees</FormDescription> */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex gap-2">
+                <FormField
+                  control={form.control}
+                  name="orders"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Orders (Monthly):</FormLabel>
+                      <FormControl>
+                        <Input type="number" min="0" {...field} />
+                      </FormControl>
+                      {/* <FormDescription>Monthly orders</FormDescription> */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="avg_order_value"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Average Order Value</FormLabel>
+                      <FormControl>
+                        <div className="flex h-10 items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2">
+                          <DollarSign className="h-[16px] w-[16px]" />
+                          <input
+                            {...field}
+                            type="number"
+                            className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+                          />
+                        </div>
+                      </FormControl>
+                      {/* <FormDescription>Average value of orders</FormDescription> */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex gap-2">
+                <FormField
+                  control={form.control}
+                  name="transaction_fee"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Transaction Fee (%):</FormLabel>
+                      <FormControl>
+                        <Input
                           type="number"
-                          className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
-                        />
-                      </div>
-                    </FormControl>
-                    {/* <FormDescription>Shopify Plus monthly fees</FormDescription> */}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="flex gap-2">
-              <FormField
-                control={form.control}
-                name="orders"
-                render={({ field }) => (
-                  <FormItem className="w-full">
-                    <FormLabel>Orders (Monthly):</FormLabel>
-                    <FormControl>
-                      <Input type="number" min="0" {...field} />
-                    </FormControl>
-                    {/* <FormDescription>Monthly orders</FormDescription> */}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="avg_order_value"
-                render={({ field }) => (
-                  <FormItem className="w-full">
-                    <FormLabel>Average Order Value</FormLabel>
-                    <FormControl>
-                      <div className="flex h-10 items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2">
-                        <DollarSign className="h-[16px] w-[16px]" />
-                        <input
+                          min="0"
+                          step=".01"
+                          inputMode="decimal"
                           {...field}
-                          type="number"
-                          className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
                         />
-                      </div>
-                    </FormControl>
-                    {/* <FormDescription>Average value of orders</FormDescription> */}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="flex gap-2">
-              <FormField
-                control={form.control}
-                name="transaction_fee"
-                render={({ field }) => (
-                  <FormItem className="w-full">
-                    <FormLabel>Transaction Fee (%):</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="number"
-                        min="0"
-                        step=".01"
-                        inputMode="decimal"
-                        {...field}
-                      />
-                    </FormControl>
-                    {/* <FormDescription>Rate of transaction fee</FormDescription> */}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="order_growth"
-                render={({ field }) => (
-                  <FormItem className="w-full">
-                    <FormLabel>Order Growth (% Year):</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="number"
-                        min="0"
-                        step=".01"
-                        inputMode="decimal"
-                        {...field}
-                      />
-                    </FormControl>
-                    {/* <FormDescription>Yearly order growth rate</FormDescription> */}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="flex gap-2">
-              <FormField
-                control={form.control}
-                name="nos_apps"
-                render={({ field }) => (
-                  <FormItem className="w-full">
-                    <FormLabel>No. of Shopify Apps:</FormLabel>
-                    <FormControl>
-                      <Input type="number" min="0" {...field} />
-                    </FormControl>
-                    {/* <FormDescription>Rate of transaction fee</FormDescription> */}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="total_app_cost"
-                render={({ field }) => (
-                  <FormItem className="w-full">
-                    <FormLabel>Total App Fees (Monthly):</FormLabel>
-                    <FormControl>
-                      <div className="flex h-10 items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2">
-                        <DollarSign className="h-[16px] w-[16px]" />
-                        <input
+                      </FormControl>
+                      {/* <FormDescription>Rate of transaction fee</FormDescription> */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="order_growth"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Order Growth (% Year):</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="number"
+                          min="0"
+                          step=".01"
+                          inputMode="decimal"
                           {...field}
-                          type="number"
-                          className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
                         />
-                      </div>
-                    </FormControl>
-                    {/* <FormDescription>Yearly order growth rate</FormDescription> */}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
+                      </FormControl>
+                      {/* <FormDescription>Yearly order growth rate</FormDescription> */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex gap-2">
+                <FormField
+                  control={form.control}
+                  name="nos_apps"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>No. of Shopify Apps:</FormLabel>
+                      <FormControl>
+                        <Input type="number" min="0" {...field} />
+                      </FormControl>
+                      {/* <FormDescription>Rate of transaction fee</FormDescription> */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="total_app_cost"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Total App Fees (Monthly):</FormLabel>
+                      <FormControl>
+                        <div className="flex h-10 items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2">
+                          <DollarSign className="h-[16px] w-[16px]" />
+                          <input
+                            {...field}
+                            type="number"
+                            className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+                          />
+                        </div>
+                      </FormControl>
+                      {/* <FormDescription>Yearly order growth rate</FormDescription> */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
 
-            <Button type="submit">Calculate My Savings</Button>
-          </form>
-        </Form>
-      ) : state?.message ? (
-        <div className="space-y-4">
-          {/* <div className="p-4 bg-green-100 text-green-700 rounded-md">
+              <Button type="submit">Calculate My Savings</Button>
+            </form>
+          </Form>
+        ) : state?.message ? (
+          <div className="space-y-4">
+            {/* <div className="p-4 bg-green-100 text-green-700 rounded-md">
             {state.message}
           </div> */}
-          <ShopifyCostBarChart
-            shopify_fees={Number(state.fields?.shopify_fees)}
-            orders={Number(state.fields?.orders)}
-            avg_order_value={Number(state.fields?.avg_order_value)}
-            transaction_fee={Number(state.fields?.transaction_fee)}
-            order_growth={Number(state.fields?.order_growth)}
-            nos_apps={Number(state.fields?.nos_apps)}
-            total_app_cost={Number(state.fields?.total_app_cost)}
-          />
-          <p className="text-2">
-            If you want to discuss how you can achieve this saving click on the
-            button below
-          </p>
-          <Button className="w-full">I want to discuss migration</Button>
-          <Button onClick={handleBack}>Back</Button>
-        </div>
-      ) : null}
+            <ShopifyCostBarChart
+              shopify_fees={Number(state.fields?.shopify_fees)}
+              orders={Number(state.fields?.orders)}
+              avg_order_value={Number(state.fields?.avg_order_value)}
+              transaction_fee={Number(state.fields?.transaction_fee)}
+              order_growth={Number(state.fields?.order_growth)}
+              nos_apps={Number(state.fields?.nos_apps)}
+              total_app_cost={Number(state.fields?.total_app_cost)}
+            />
+            <p className="text-2">
+              If you want to discuss how you can achieve this saving click on
+              the button below
+            </p>
+            <Button className="w-full">I want to discuss migration</Button>
+            <Button onClick={handleBack}>Back</Button>
+          </div>
+        ) : null}
+      </div>
     </>
   );
 };
